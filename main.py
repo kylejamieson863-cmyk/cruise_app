0bdeed42c808b2952535dd59c110ce08119e88b9
kylejamieson863-cmyk <kylejamieson863@gmail.com>
2026-07-22 20:53:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ if selected_deck == 5:
         camera_hotspots = [
             {"id": "sorrentos", "x": 31, "y": 41, "photo": "sorrentos.jpg"},
             {"id": "pearl", "x": 30, "y": 51, "photo": "the_pearl.jpg"},
-            {"id": "pearl", "x": 50, "y": 51, "photo": "the_pearl_1.jpg"},
+            {"id": "pearl", "x": 50, "y": 51, "photo": "pearl.jpg"},
             {"id": "duck", "x": 40, "y": 62, "photo": "the_duck.jpg"},
             {"id": "dog", "x": 51, "y": 60, "photo": "dog.jpg"},
         ]
