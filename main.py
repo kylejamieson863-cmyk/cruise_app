fd38b9f51250417a1c7eb595cfcffda70b984224
kylejamieson863-cmyk <kylejamieson863@gmail.com>
2026-07-26 15:57:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -272,6 +272,7 @@ deck_data = {
     ],
     7: [
         {"id": "slide_1", "x": 46, "y": 85, "file": "slide_1.jpg"},
+        {"id": "sweetshop", "x": 46, "y": 85, "file": "sweetshop_7.jpg"},
     ],
     8: [],
     9: [],
